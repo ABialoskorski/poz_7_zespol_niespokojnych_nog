73187dba69a3b09b9aeab51edc7d3bb344de8203
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-04-13 07:29:21 +0200

    baranek łap

--- backend/routers/api.js
+++ backend/routers/api.js
@@ -31,6 +31,36 @@ router.get('/search', ensureToken, async (req, res) => {
   }
 })
 
+router.get('/rekurencja', ensureToken, async (req, res) => {
+  try {
+    const sum = []
+    const queue = ['2']
+    while (queue.length) {
+      const url = 'https://api.allegro.pl/sale/categories'
+      const { data } = await axios.get(url, {
+        params: {
+          'parent.id': queue.shift()
+        },
+        headers: {
+          Authorization: 'Bearer ' + req.token.access_token,
+          Accept: 'application/vnd.allegro.public.v1+json'
+        }
+      })
+      Array.prototype.push.apply(sum, (data.categories.map(c => {
+        return {
+          id: c.id,
+          name: c.name
+        }
+      })))
+      Array.prototype.push.apply(queue, (data.categories.map(c => c.id)))
+    }
+    return res.json(sum)
+  } catch (error) {
+    console.error('Request error', error.message)
+    return res.json(error.message)
+  }
+})
+
 router.get('/categories', ensureToken, async (req, res) => {
   try {
     const url = 'https://api.allegro.pl/sale/categories'
