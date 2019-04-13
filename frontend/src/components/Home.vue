1e44545646c84ce1507e84e69dc21c60c0ef924e
KonradBaranek <konrad.baranek01@gmail.com>
2019-04-13 02:29:02 +0200

    added modal

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,33 +1,28 @@
 <template>
-  <div class="home">
-    <h1 class="home__title">Komponenty</h1>
-    <v-container class="my-5">
-      <h2 class="grey--text">Komponenty wymagane</h2>
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3 v-for="category in required(categories,true)" :key="category.name">
-          <v-card flat class="text-xs-center ma-3">
-            <v-responsive class="pt-4">
-              <v-avatar size="90" class="grey lighten-2">
-                <img :src="category.avatar">
-              </v-avatar>
-            </v-responsive>
-            <v-card-text>
-              <div class="subheading">{{ category.name }}</div>
-              <div class="grey--text">{{ category.description }}</div>
-            </v-card-text>
-
-            <v-btn color="primary" dark @click.stop="dialog = true">Open</v-btn>
-            <v-dialog v-model="dialog" max-width="300">
-              <v-card>
-                <v-card-title class="headline">Wybierz</v-card-title>
-                <v-card-text>text</v-card-text>
-                <v-card-actions></v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
+  <div>
+    <div class="home">
+      <h1 class="home__title">Komponenty</h1>
+      <v-container class="my-5">
+        <h2 class="grey--text">Komponenty wymagane</h2>
+        <v-layout row wrap>
+          <v-flex xs12 sm6 md4 lg3 v-for="category in required(categories,true)" :key="category.name">
+            <v-card flat class="text-xs-center ma-3">
+              <v-responsive class="pt-4">
+                <v-avatar size="90" class="grey lighten-2">
+                  <img :src="category.avatar">
+                </v-avatar>
+              </v-responsive>
+              <v-card-text>
+                <div class="subheading">{{ category.name }}</div>
+                <div class="grey--text">{{ category.description }}</div>
+              </v-card-text>
+              <v-card-actions class="right">
+                <v-btn color="primary" dark @click="dialog = true">Wyszukaj</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
 
       <v-container class="my-5">
         <h2 class="grey--text">Komponenty opcjonalne</h2>
