e45b081e342ad15037d74baf531c54608a9fe73f
Artur Białoskórski <bialoskorski.artur@gmail.com>
2019-04-13 05:59:57 +0200

    home and products components

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/Home.vue";
+import Products from "@/components/Products.vue";
 
 Vue.use(Router);
 
@@ -8,8 +9,14 @@ export default new Router({
 	mode: "history",
 	base: process.env.BASE_URL,
 	routes: [{
-		path: "/",
-		name: "home",
-		component: Home,
-	}, ],
+			path: "/",
+			name: "home",
+			component: Home,
+		},
+		{
+			path: "/products",
+			name: "products",
+			component: Products,
+		},
+	],
 });
