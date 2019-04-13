a1850464908e2e440e51694b373157105322b8a6
Artur Białoskórski <bialoskorski.artur@gmail.com>
2019-04-13 13:35:38 +0200

    fonts added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,7 +21,4 @@ export default {
 </script>
 
 <style>
-  font-family: 'Amiko', sans-serif;
-  font-family: 'Rubik', sans-serif;
-
 </style>
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,17 +1,7 @@
 <template>
   <v-toolbar class="navbar" fixed color="primary white--text">
-    <v-btn fab small :to="{
-        name: 'home'
-        }">
-      <v-icon>home</v-icon>
-    </v-btn>
     <v-spacer></v-spacer>
     <span class="navbar__price">Suma: {{this.allPrice}} zł</span>
-    <v-toolbar-items>
-      <v-btn class="white--text" flat :to="{
-        name: 'products'
-        }">Konfigurator</v-btn>
-    </v-toolbar-items>
   </v-toolbar>
 </template>
 
@@ -32,6 +22,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.navbar {
+  font-family: "Amika", sans-serif;
+}
 .navbar__price {
   font-size: 24px;
   margin-right: 40px;
